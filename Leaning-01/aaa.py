f6216db1f6540d2ace264125ec384bcb03ccc273
xiao-wu-z <2721059902@qq.com>
2025-09-13 09:57:46 +0800

    Leaning

--- Leaning-01/aaa.py
+++ Leaning-01/aaa.py
@@ -1,4 +1,7 @@
-from pywinauto import Application
+import math
+import time
+from pywinauto import Application,mouse
+
 
 
 # #start启动应用程序
@@ -24,3 +27,33 @@ from pywinauto import Application
 # print("max result:",win.is_maximized())
 #
 # win.close()
+
+app = Application(backend="uia").connect(process=28224)
+
+win = app.window(title = "抖音")
+
+win.wait("visible")
+
+# liked_group = win.child_window(title="16.1万", control_type="Group")
+#
+# # 获取liked_group子控件
+# like_icon = liked_group.children()[0]
+# # 点赞
+# like_icon.click_input()
+#
+# point = win.rectangle().mid_point()
+#
+# mouse.scroll(coords=(point.x, point.y), wheel_dist=-500)
+#
+# win.print_control_identifiers()
+
+for i in range(0,3):
+    point = win.rectangle().mid_point()
+    time.sleep(2)
+
+    # win.double_click_input()
+
+    mouse.double_click(coords=(point.x, point.y))
+    time.sleep(2)
+
+    mouse.scroll(coords=(point.x, point.y), wheel_dist=-500)
